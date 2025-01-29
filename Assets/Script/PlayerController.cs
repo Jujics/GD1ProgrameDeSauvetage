9a9b0fc9a533cd1980da84bd52f405ac2c5b393f
Juji.cs <61978075+Jujics@users.noreply.github.com>
2025-01-29 21:59:50 +0100

    Update PlayerController.cs

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,16 +9,21 @@ public class PlayerController : MonoBehaviour
     
     void Start()
     {
+        //recupere le composant sur le joueur
         rb = GetComponent<Rigidbody>();
     }
     
     void FixedUpdate()
     {
+        //recupere les input du joueur
         float moveHorizontal = Input.GetAxis("Horizontal");
+        // boutons z et s
         float moveVertical = Input.GetAxis("Vertical");
+        //boutons q et d
         
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         
+        //applique le mouvement
         rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
     }
 }
